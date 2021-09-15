ff5a45206be7fbfbe1fb7508cb57b2984dd838dc
kimberly lin <kimberly.lin10@gmail.com>
2021-09-15 12:27:14 -0700

    created base template and built user list page. Also built users in models

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template
+from models import db, connect_db, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -10,3 +10,8 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+@app.get("/")
+def load_home():
+    users=User.query.all()
+    return render_template("user_list.html", users = users)
